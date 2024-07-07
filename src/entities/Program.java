5ad5f43368ff38d77c8720a243a4558dd985a156
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-07 02:10:39 -0300

    Resolvido problema crescente

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -1,9 +1,34 @@
 package entities;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		// Problema "crescente"
+
+		int X;
+		int Y;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite dois numeros: ");
+		X = sc.nextInt();
+		Y = sc.nextInt();
+
+		while (X != Y) {
+			if (X > Y) {
+				System.out.println("DECRESCENTE!");
+			} else {
+				System.out.println("CRESCENTE!");
+			}
+			System.out.println("Digite outros dois numeros: ");
+			X = sc.nextInt();
+			Y = sc.nextInt();
+
+		}
+
+		sc.close();
 
 	}
 
